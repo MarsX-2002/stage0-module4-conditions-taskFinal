36b6fa46a3205bf5b758224b9358524a72c8cc41
MarsX-2002 <mirjalol0331@gmail.com>
2023-03-30 10:08:58 +0500

    operators, if else, switch

--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,12 +2,17 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
-        int result = dividend / divider;
-        result *= divider;
-        if (result == dividend){
-            System.out.println("can be divided completely");
+
+        if (divider != 0) {
+            int result = dividend / divider;
+            result *= divider;
+            if (result == dividend) {
+                System.out.println("can be divided completely");
+            } else {
+                System.out.println("cannot be divided completely");
+            }
         } else {
-            System.out.println("cannot be divided completely");
+            System.out.println("division by zero");
         }
     }
 }
