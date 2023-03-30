1feb8b27906143df55266738e883fed0414dfe4b
MarsX-2002 <mirjalol0331@gmail.com>
2023-03-30 10:30:29 +0500

    operators, if else, switch

--- src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
@@ -2,15 +2,13 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class TriangleSidesValidator {
     public void validate(double firstSide, double secondSide, double thirdSide) {
-        double side1 = Math.sqrt(Math.pow(firstSide - secondSide, 2) + Math.pow(firstSide - thirdSide, 2));
-        double side2 = Math.sqrt(Math.pow(secondSide - firstSide, 2) + Math.pow(secondSide - thirdSide, 2));
-        double side3 = Math.sqrt(Math.pow(thirdSide - firstSide, 2) + Math.pow(thirdSide - secondSide, 2));
-
-        // Check the Triangle Inequality Theorem
-        if (side1 + side2 > side3 && side2 + side3 > side1 && side3 + side1 > side2) {
-            System.out.println("This is a valid triangle.");
+        if (firstSide > 0 && secondSide > 0 && thirdSide > 0 && (firstSide + secondSide > thirdSide &&
+                firstSide + thirdSide > secondSide &&
+                thirdSide + secondSide > firstSide)
+        ) {
+            System.out.println("this is a valid triangle");
         } else {
-            System.out.println("It's not a triangle.");
+            System.out.println("it's not a triangle");
         }
     }
 }
