df3857d40e2a4c7ec01df401a7231b800664a029
MarsX-2002 <mirjalol0331@gmail.com>
2023-03-30 09:54:32 +0500

    operators, if else, switch

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -1,6 +1,17 @@
 package school.mjc.stage0.conditions.finalTask;
 
 public class BitwiseValuesSwap {
-    public void swap(int first, int second) {
+    public static void swap(int first, int second) {
+//first  1 = 01  -> 2
+//second 2 = 10  -> 1
+        first = first ^ second; // 11
+        second = first ^ second; // 01
+        first = first ^ second; // 10
+        System.out.println(first);
+        System.out.println(second);
+    }
+
+    public static void main(String[] args) {
+        swap(1, 2);
     }
 }
